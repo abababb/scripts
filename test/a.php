a5bb4b78196f74f3dd9409ce4b28764bdf304ce0
abababb <tenkanse@yahoo.cn>
2012-12-14 14:55:23 +0800

    my wordpress

--- test/a.php
+++ test/a.php
@@ -1,3 +1,12 @@
+<pre>
 <?php
-echo phpinfo();
+$date = new DateTime('2000-12-31');
+$interval = new DateInterval('P1M');
+
+$date->add($interval);
+echo $date->format('Y-m-d') . "\n";
+
+$date->add($interval);
+echo $date->format('Y-m-d') . "\n";
 ?>
+</pre>
