06f7f735f43bac9d435f940cb1a3bdf6b46978c7
abababb <tenkanse@yahoo.cn>
2013-03-04 17:29:06 +0800

    123

--- test/a.php
+++ test/a.php
@@ -1,12 +1,15 @@
 <?php
-$people = new ArrayIterator(array('John', 'Jane', 'Jack', 'Judy'));
-$roles = new ArrayIterator(array('Developer', 'Scrum Master', 'Project Owner'));
-
-$team = new MultipleIterator();
-$team->attachIterator($people, 'person');
-$team->attachIterator($roles, 'role');
-
-foreach ($team as $member) {
-    print_r($member);
+$file = '';
+$html = '';
+for ($i = 1; $i < 2; $i++) {
+    $file .= file_get_contents("http://tieba.baidu.com/f/search/ures?kw=linux&qw=&rn=10&un=%CE%E1%C4%CB%C0%CFw&sm=1&sd=&ed=&pn=$i");
 }
+preg_match_all("/<span class=\"p_title\">(?s).*?<\/span>/", $file, $out, PREG_SET_ORDER);
+
+foreach ($out as $val) {
+    preg_match_all("/(?!a)*/", $val[0], $html, PREG_SET_ORDER);
+    $result .= $html;
+}
+
+echo $result;
 ?>
