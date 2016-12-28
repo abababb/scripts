9d463213d038a812f8069fb1b73aead263d6feef
zzz <tenkanse@hotmail.com>
2016-12-28 16:34:26 +0800

    php7的spaceship操作符配合usort多条件连锁排序例子

---
+++ 2016/example/spaceship_operator_chaining.php
@@ -0,0 +1,70 @@
+<?php
+
+$testArray = array(
+    array(
+        'high' => '3',//高优先级条件
+        'low' => '30',//低优先级条件
+    ),
+    array(
+        'high' => '3',
+        'low' => '20',
+    ),
+    array(
+        'high' => '2',
+        'low' => '40',
+    ),
+    array(
+        'high' => '2',
+        'low' => '10',
+    ),
+    array(
+        'high' => '1',
+        'low' => '8',
+    ),
+    array(
+        'high' => '1',
+        'low' => '58',
+    ),
+);
+
+//先按高优先级条件从低到高，再按低优先级条件从高到低排序
+usort($testArray, function ($item1, $item2) {
+    return $item1['high'] <=> $item2['high'] ?: $item2['low'] <=> $item1['low'];
+});
+
+var_export($testArray);exit;
+
+$sortResult = 
+    array (
+        0 => 
+        array (
+            'high' => '1',
+            'low' => '58',
+        ),
+        1 => 
+        array (
+            'high' => '1',
+            'low' => '8',
+        ),
+        2 => 
+        array (
+            'high' => '2',
+            'low' => '40',
+        ),
+        3 => 
+        array (
+            'high' => '2',
+            'low' => '10',
+        ),
+        4 => 
+        array (
+            'high' => '3',
+            'low' => '30',
+        ),
+        5 => 
+        array (
+            'high' => '3',
+            'low' => '20',
+        ),
+    )
+    ;
